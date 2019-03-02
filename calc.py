73fa229dcd60e775db42127c322767c4f44bc526
Ashwin Ramesh <ashwinr64@gmail.com>
2019-03-02 14:17:55 +0530

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,14 @@
+while True:
+    print("1.Add\n2.Subtract\n3.Divide\n4.Multiply")
+    op = int(input(Enter choice:))
+    input_vals()
+    if op == 1:
+        print(add(a, b))
+    elif op == 2:
+        print(sub(a, b))
+    elif op == 3:
+        print(divide(a, b))
+    elif op == 4:
+        print(multiply(a, b))
+    else:
+        break
