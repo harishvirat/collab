b9b48dd30037d512861dcedd9ef736259d098171
Ashwin Ramesh <ashwinr64@gmail.com>
2019-03-02 14:45:41 +0530

    refactor

--- calc.py
+++ calc.py
@@ -1,25 +1,27 @@
-while True:
-    print("1.Add\n2.Subtract\n3.Divide\n4.Multiply")
-    op = int(input(Enter choice:))
-    input_vals()
-    if op == 1:
-        print(add(a, b))
-    elif op == 2:
-        print(sub(a, b))
-    elif op == 3:
-        print(divide(a, b))
-    elif op == 4:
-        print(multiply(a, b))
-    else:
-        break
-
 def add(a,b):
     return a + b
+
 def sub(a,b):
     return a - b
 
 def multiply(a,b):
     return a * b
 
-def divide(a,b):
-    return a / b
+while True:
+    print("1.Add\n2.Subtract\n3.Divide\n4.Multiply")
+    op = int(input("Enter choice:"))
+
+    if op == 1:
+        a = int(input("Enter a: "))
+        b = int(input("Enter b: "))
+        print(add(a, b))
+    elif op == 2:
+        a = int(input("Enter a: "))
+        b = int(input("Enter b: "))
+        print(sub(a, b))
+    elif op == 4:
+        a = int(input("Enter a: "))
+        b = int(input("Enter b: "))
+        print(multiply(a, b))
+    else:
+        break
